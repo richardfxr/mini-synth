dc0c18bf62ba637e3b1948653f63347e9c8368de
Richard Fu <richardfxr.personal@gmail.com>
2023-04-11 19:38:04 -0400

    🟠SVLT: add reels component

---
+++ src/lib/reels.svelte
@@ -0,0 +1,114 @@
+<script lang="ts">
+    import type { Tweened } from 'svelte/motion';
+    /* === IMPORTS ============================ */
+    import type * as Tone from 'tone';
+
+    /* === PROPS ============================== */
+    export let playbackState: Tone.PlaybackState;
+    export let tweenedProgress: Tweened<number>;
+    export let subdivWidth: number;
+    export let currentSubdiv: number; // bind
+    export let melody: Tone.Unit.Frequency[][];
+    export let hasManuallyScrolled: boolean; // bind
+
+    tweenedProgress.subscribe(() => {
+        // set scrollLeft using progress (tweened)
+        if (playbackState === "started") {
+            // console.log("progress: " + $progress);
+            tapes.scrollLeft = $tweenedProgress;
+        }
+    });
+
+    /* === VARIABLES ========================== */
+    let tapes: HTMLElement;
+</script>
+
+
+
+<div class="reels">
+    <div
+        class="tapes"
+        bind:this={tapes}
+        on:scroll={() => {
+            // update progress if it is not updating scroll
+            if (playbackState !== "started") {
+                tweenedProgress.set(tapes.scrollLeft);
+                currentSubdiv = Math.floor(tapes.scrollLeft / subdivWidth);
+                hasManuallyScrolled = true;
+            }
+        }}>
+        <div class="trackPadding"></div>
+        <div
+            class="tracks"
+            style="--melodyLength: {melody.length}">
+            {#each melody as subdiv, i}
+                <div
+                    class="subdiv"
+                    class:active={i === currentSubdiv}>
+                    {#each subdiv as note}
+                        <p>{note}</p>
+                    {/each}
+                </div>
+            {/each}
+        </div>
+        <div class="trackPadding"></div>
+    </div>
+</div>
+
+
+
+<style lang="scss">
+    .reels {
+        position: relative;
+        padding: 10px 0;
+        
+        &::before {
+            content: "";
+            position: absolute;
+            top: 0;
+            bottom: 0;
+            left: calc(50% - 0.5px);
+            width: 1px;
+            z-index: 1000;
+            background-color: red;
+        }
+    }
+
+    .tapes {
+        display: flex;
+        flex-flow: row nowrap;
+        position: relative;
+        overflow-x: auto;
+        overflow-y: hidden;
+    }
+
+    .trackPadding {
+        width: 50%;
+        flex-shrink: 0;
+    }
+
+    .tracks {
+        display: grid;
+        grid-template-columns: repeat(var(--melodyLength), 50px);
+        background-color: var(--clr-100);
+
+        .subdiv {
+            display: flex;
+            flex-direction: column;
+            gap: 1px;
+            padding: 10px 0.5px;
+
+            &.active {
+                background-color: var(--clr-150);
+            }
+
+            p {
+                color: var(--clr-0);
+
+                background-color: var(--clr-800);
+                padding: 3px;
+                border-radius: 2px;
+            }
+        }
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,46 +5,105 @@
     import { tweened } from 'svelte/motion';
 	import { cubicOut } from 'svelte/easing';
     import * as Tone from 'tone';
-    import type { BPM } from 'tone/build/esm/core/type/Units';
+    import Reels from '../lib/reels.svelte';
 
     /* === CONSTANTS ========================== */
     const subdivWidth = 50;
     const tweenDuration = 100;
-    const progress = tweened(0, {
+    const tweenedProgress = tweened(0, {
 		duration: tweenDuration,
 		easing: cubicOut
 	});
 
     /* === VARIABLES ========================== */
+    let playbackState: Tone.PlaybackState = "stopped";
+    let playbackProgress: number = 0;
+
     let synth: Tone.PolySynth;
-    let bpm: BPM = 80;
-    let melody: Tone.Unit.Frequency[][] = [];
+    let bpm: Tone.Unit.BPM = 80;
+    let melody: Tone.Unit.Frequency[][] = [
+        ["E3", "E5"],
+        [],
+        ["G3", "B3"],
+        ["B4"],
+        [],
+        [],
+        ["G3", "B3"],
+        ["A4"],
+        ["E3", "G4"],
+        ["A4"],
+        ["G3", "B3"],
+        ["B4"],
+        [],
+        ["G4"],
+        ["G3", "B3", "A4"],
+        ["D5"],
+
+        ["G3"],
+        [],
+        ["B3", "D4"],
+        ["B4"],
+        [],
+        [],
+        ["B3", "D4"],
+        ["A4"],
+        ["G3", "G4"],
+        ["A4"],
+        ["B3", "D4"],
+        ["Gb4"],
+        [],
+        ["G4"],
+        ["B3", "D4", "A4"],
+        ["D5"],
+
+        ["Gb3"],
+        [],
+        ["B3", "D4"],
+        ["B4"],
+        [],
+        [],
+        ["B3", "D4"],
+        ["D5"],
+        ["Gb3", "C5"],
+        ["B4"],
+        ["B3", "D4"],
+        ["A4"],
+        [],
+        ["G4"],
+        ["B3", "D4", "A4"],
+        ["E4"],
+    ];
     let melodyPart: Tone.Part;
     let notesToPlay: {time: Tone.Unit.Time, note: Tone.Unit.Frequency[], index: number}[] = [];
+    let tapesFrame: number;
 
-    let trackContainer: HTMLElement;
-    let trackFrame: number;
     let currentSubdiv: number = 0;
     
-    let autoScroll = false;
+    // let autoScroll = false;
     let hasManuallyScrolled = false;
-    
-    /* === FUNCTIONS ========================== */
-    function scrollTrack(): void {
-        console.log("Tone.Transport.state: " + Tone.Transport.state);
-        if (!browser || !trackContainer || Tone.Transport.state !== "started") return;
 
-        console.log("scrolling");
+    /* === FUNCTIONS ========================== */
+    function scrollTapes(): void {
+        if (!browser) return;
+
+        playbackState = Tone.Transport.state;
+        playbackProgress = Tone.Transport.progress;
+
+        if (playbackState !== "started") return;
 
         const trackLength = melody.length * subdivWidth;
-        progress.set(Tone.Transport.progress * trackLength);
+        tweenedProgress.set(playbackProgress * trackLength);
 
         // console.log("progress: " + Tone.Transport.progress);
         // console.log("trackLength: " + trackLength);
         // console.log("srollLeft: " + Tone.Transport.progress * trackLength);
 
-        trackFrame = requestAnimationFrame(scrollTrack);
+        tapesFrame = requestAnimationFrame(scrollTapes);
     };
+
+    function startTapes(): void {
+        tapesFrame = requestAnimationFrame(scrollTapes);
+    }
 
     /* === LIFECYCLES ========================= */
     onMount(() => {
@@ -57,59 +116,6 @@
 
         // set transporter properties
         Tone.Transport.bpm.value = bpm;
-
-        melody = [
-            ["E3", "E5"],
-            [],
-            ["G3", "B3"],
-            ["B4"],
-            [],
-            [],
-            ["G3", "B3"],
-            ["A4"],
-            ["E3", "G4"],
-            ["A4"],
-            ["G3", "B3"],
-            ["B4"],
-            [],
-            ["G4"],
-            ["G3", "B3", "A4"],
-            ["D5"],
-
-            ["G3"],
-            [],
-            ["B3", "D4"],
-            ["B4"],
-            [],
-            [],
-            ["B3", "D4"],
-            ["A4"],
-            ["G3", "G4"],
-            ["A4"],
-            ["B3", "D4"],
-            ["Gb4"],
-            [],
-            ["G4"],
-            ["B3", "D4", "A4"],
-            ["D5"],
-
-            ["Gb3"],
-            [],
-            ["B3", "D4"],
-            ["B4"],
-            [],
-            [],
-            ["B3", "D4"],
-            ["D5"],
-            ["Gb3", "C5"],
-            ["B4"],
-            ["B3", "D4"],
-            ["A4"],
-            [],
-            ["G4"],
-            ["B3", "D4", "A4"],
-            ["E4"],
-        ];
 
         melody.forEach((subdiv, index) =>{
             const time = "0:0:" + index;
@@ -130,50 +136,21 @@
         Tone.Transport.loop = true;
         Tone.Transport.loopEnd = "0:0:" + notesToPlay.length;
 
-        progress.subscribe(() => {
-            // set scrollLeft using progress (tweened)
-            if (autoScroll) {
-                // console.log("progress: " + $progress);
-                trackContainer.scrollLeft = $progress;
-            }
-        });
-
         return () => {
-            cancelAnimationFrame(trackFrame);
+            // cancel tapesFrame on destroy
+            cancelAnimationFrame(tapesFrame);
         };
     });
 </script>
 
 
-<div class="playerContainer">
-    <div
-        class="trackContainer"
-        bind:this={trackContainer}
-        on:scroll={() => {
-            // update progress if it is not updating scroll
-            if (!autoScroll) {
-                progress.set(trackContainer.scrollLeft);
-                currentSubdiv = Math.floor(trackContainer.scrollLeft / subdivWidth);
-                hasManuallyScrolled = true;
-            }
-        }}>
-        <div class="trackPadding"></div>
-        <div
-            class="tracks"
-            style="--melodyLength: {melody.length}">
-            {#each melody as subdiv, i}
-                <div
-                    class="subdiv"
-                    class:active={i === currentSubdiv}>
-                    {#each subdiv as note}
-                        <p>{note}</p>
-                    {/each}
-                </div>
-            {/each}
-        </div>
-        <div class="trackPadding"></div>
-    </div>
-</div>
+<Reels
+    {playbackState}
+    {tweenedProgress}
+    {subdivWidth}
+    bind:currentSubdiv = {currentSubdiv}
+    {melody}
+    bind:hasManuallyScrolled = {hasManuallyScrolled} />
 
 <div class="controls">
     <button on:click={async () => {
@@ -185,20 +162,16 @@
         } else {
             Tone.Transport.start();
         }
-        
-        autoScroll = true;
-        console.log("begins at: " + trackContainer.scrollLeft);
-        trackFrame = requestAnimationFrame(scrollTrack);
+
+        startTapes();
+
+        console.log("Tone.Transport.state: " + Tone.Transport.state);
     }}>
         Play
     </button>
 
     <button on:click={async () => {
         Tone.Transport.pause();
-        setTimeout(() => {
-            autoScroll = false;
-            hasManuallyScrolled = false;
-        }, tweenDuration);
     }}>
         Pause
     </button>
@@ -207,60 +180,6 @@
 
 
 <style lang="scss">
-    .playerContainer {
-        position: relative;
-        padding: 10px 0;
-        
-        &::before {
-            content: "";
-            position: absolute;
-            top: 0;
-            bottom: 0;
-            left: calc(50% - 0.5px);
-            width: 1px;
-            z-index: 1000;
-            background-color: red;
-        }
-    }
-
-    .trackContainer {
-        display: flex;
-        flex-flow: row nowrap;
-        position: relative;
-        overflow-x: auto;
-        overflow-y: hidden;
-    }
-
-    .trackPadding {
-        width: 50%;
-        flex-shrink: 0;
-    }
-
-    .tracks {
-        display: grid;
-        grid-template-columns: repeat(var(--melodyLength), 50px);
-        background-color: var(--clr-100);
-
-        .subdiv {
-            display: flex;
-            flex-direction: column;
-            gap: 1px;
-            padding: 10px 0.5px;
-
-            &.active {
-                background-color: var(--clr-150);
-            }
-
-            p {
-                color: var(--clr-0);
-
-                background-color: var(--clr-800);
-                padding: 3px;
-                border-radius: 2px;
-            }
-        }
-    }
-
     .controls {
         display: flex;
         flex-direction: row;
