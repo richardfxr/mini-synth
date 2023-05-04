2fc6c9ca3e27b86bbd27ea6886d23f2125a736da
Richard Fu <richardfxr.personal@gmail.com>
2023-05-04 14:30:55 -0400

    📐DES: add cancel icon

---
+++ src/lib/SVGs/cancelIcon.svelte
@@ -0,0 +1,9 @@
+<svg
+    class="cancel icon"
+    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18">
+    <polygon fill="currentColor" points="15.71 3.71 14.29 2.29 9 7.59 3.71 2.29 2.29 3.71 7.59 9 2.29 14.29 3.71 15.71 9 10.41 14.29 15.71 15.71 14.29 10.41 9 15.71 3.71"/>
+</svg>
+
+
+
+<style lang="scss"></style>
