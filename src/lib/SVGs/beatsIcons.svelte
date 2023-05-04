29a41e5c18981f4fca6ef20aa5ba76a06715a0e5
Richard Fu <richardfxr.personal@gmail.com>
2023-05-04 15:09:47 -0400

    📐DES: add piano & beats icons

---
+++ src/lib/SVGs/beatsIcons.svelte
@@ -0,0 +1,13 @@
+<svg
+    class="beats icon"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 18 18">
+    <rect fill="currentColor" width="8" height="8" rx="1" ry="1"/>
+    <rect fill="currentColor" x="10" width="8" height="8" rx="1" ry="1"/>
+    <rect fill="currentColor" y="10" width="8" height="8" rx="1" ry="1"/>
+    <rect fill="currentColor" x="10" y="10" width="8" height="8" rx="1" ry="1"/>
+</svg>
+
+
+
+<style lang="scss"></style>
---
+++ src/lib/SVGs/pianoIcon.svelte
@@ -0,0 +1,12 @@
+<svg
+    class="piano icon"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 18 18">
+    <path fill="currentColor" d="m14.5,0v9h-1.25v9h3.75c.55,0,1-.45,1-1V1c0-.55-.45-1-1-1h-2.5Z"/>
+    <path fill="currentColor" d="m3.5,9V0H1C.45,0,0,.45,0,1v16c0,.55.45,1,1,1h3.75v-9h-1.25Z"/>
+    <polygon fill="currentColor" points="10 9 10 0 8 0 8 9 6.75 9 6.75 18 11.25 18 11.25 9 10 9"/>
+</svg>
+
+
+
+<style lang="scss"></style>
