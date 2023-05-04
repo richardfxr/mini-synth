ef38a74bfdbc311d15cb5101c0f9ed767f7629ca
Richard Fu <richardfxr.personal@gmail.com>
2023-05-04 16:25:45 -0400

    📐DES: add tapeArrows icon

---
+++ src/lib/SVGs/tapeArrows.svelte
@@ -0,0 +1,15 @@
+<svg
+    class="tapeArrows"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 30 17">
+    <path fill="currentColor" d="m30,6H10V1.1c0-.4-.22-.76-.58-.94-.36-.18-.78-.14-1.1.1L.52,6.11c-.36.27-.51.74-.37,1.17.14.43.54.72,1,.72h28.85v-2Z"/>
+    <path fill="currentColor" d="m30,15H10v-4.9c0-.4-.22-.76-.58-.94-.36-.18-.78-.14-1.1.1L.52,15.11c-.36.27-.51.74-.37,1.17.14.43.54.72,1,.72h28.85v-2Z"/>
+</svg>
+
+
+
+<style lang="scss">
+    .tapeArrows {
+        width: 100%;
+    }
+</style>
