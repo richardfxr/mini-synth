a9e890d1775a025c714e6d1cc2531650d87de8b7
Richard Fu <richardfxr.personal@gmail.com>
2023-05-11 15:15:27 -0400

    💄CSS: remove .reels .isReady for troubleshooting

--- src/lib/reels.svelte
+++ src/lib/reels.svelte
@@ -170,8 +170,11 @@
                     opacity $cassette-ani-duration $cassette-ani-easing;
         
         // load state
-        transform: translateY(calc(-1 * var(--reels-height) - var(--tapeMarker-height) + var(--cassettTop-translateY)));
-        opacity: 0;
+        // transform: translateY(calc(-1 * var(--reels-height) - var(--tapeMarker-height) + var(--cassettTop-translateY)));
+        // opacity: 0;
+
+        transform: translateY(0);
+        opacity: 1;
 
         &::before, &::after {
             // playhead
@@ -223,7 +226,7 @@
         transition: transform var(--trans-slow) var(--trans-cubic-1);
 
         // load state
-        transform: translateX(100px);
+        transform: translateX(0);
 
         &.dragging {
             cursor: grabbing;
@@ -334,16 +337,16 @@
         }
     }
 
-    .reels.isReady {
-        // default state
-        transform: translateY(0);
-        opacity: 1;
+    // .reels.isReady {
+    //     // default state
+    //     transform: translateY(0);
+    //     opacity: 1;
 
-        .spools {
-            // default state
-            transform: translateX(0);
-        }
-    }
+    //     .spools {
+    //         // default state
+    //         transform: translateX(0);
+    //     }
+    // }
 
     /* === COLOR SCHEME ======================= */
     @media (prefers-color-scheme: dark) {
