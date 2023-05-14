8df589cd05d782e1959cb7a2a209d4a820bddc39
Richard Fu <richardfxr.personal@gmail.com>
2023-05-14 16:07:47 -0400

    📐DES: add fullscreen icons

---
+++ src/lib/SVGs/enterFullscreenIcon.svelte
@@ -0,0 +1,13 @@
+<svg
+    class="enterFullscreen icon"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 18 18">
+    <path fill="currentColor" d="m2,7H0V1.5C0,.67.67,0,1.5,0h5.5v2H2v5Z"/>
+    <path fill="currentColor" d="m18,7h-2V2h-5V0h5.5c.83,0,1.5.67,1.5,1.5v5.5Z"/>
+    <path fill="currentColor" d="m7,18H1.5c-.83,0-1.5-.67-1.5-1.5v-5.5h2v5h5v2Z"/>
+    <path fill="currentColor" d="m16.5,18h-5.5v-2h5v-5h2v5.5c0,.83-.67,1.5-1.5,1.5Z"/>
+</svg>
+
+
+
+<style lang="scss"></style>
---
+++ src/lib/SVGs/exitFullscreen.svelte
@@ -0,0 +1,13 @@
+<svg
+    class="exitFullscreen icon"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 18 18">
+    <path fill="currentColor" d="m13,18h-2v-5.5c0-.83.67-1.5,1.5-1.5h5.5v2h-5v5Z"/>
+    <path fill="currentColor" d="m7,18h-2v-5H0v-2h5.5c.83,0,1.5.67,1.5,1.5v5.5Z"/>
+    <path fill="currentColor" d="m18,7h-5.5c-.83,0-1.5-.67-1.5-1.5V0h2v5h5v2Z"/>
+    <path fill="currentColor" d="m5.5,7H0v-2h5V0h2v5.5c0,.83-.67,1.5-1.5,1.5Z"/>
+</svg>
+
+
+
+<style lang="scss"></style>
