7f0c6d3248b28fa0f9a729ad29c1e216b461ebe9
Richard Fu <richardfxr.personal@gmail.com>
2023-05-14 22:07:16 -0400

    📐DES: add sun & moon icons

---
+++ src/lib/SVGs/sunIcon.svelte
@@ -0,0 +1,18 @@
+<svg
+    class="sun icon"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 18 18">
+        <path fill="currentColor" d="m9,3c.34,0,.67.04,1,.09V0h-2v3.09c.33-.05.66-.09,1-.09Z"/>
+        <path fill="currentColor" d="m9,13c-2.21,0-4-1.79-4-4s1.79-4,4-4,4,1.79,4,4-1.79,4-4,4Zm0-6c-1.1,0-2,.9-2,2s.9,2,2,2,2-.9,2-2-.9-2-2-2Z"/>
+        <path fill="currentColor" d="m5.53,4.11L3.34,1.93l-1.41,1.41,2.18,2.18c.39-.55.87-1.02,1.41-1.41Z"/>
+        <path fill="currentColor" d="m3,9c0-.34.04-.67.09-1H0v2h3.09c-.05-.33-.09-.66-.09-1Z"/>
+        <path fill="currentColor" d="m4.11,12.47l-2.18,2.18,1.41,1.41,2.18-2.18c-.55-.39-1.02-.87-1.41-1.41Z"/>
+        <path fill="currentColor" d="m9,15c-.34,0-.67-.04-1-.09v3.09h2v-3.09c-.33.05-.66.09-1,.09Z"/>
+        <path fill="currentColor" d="m13.89,12.47c-.39.55-.87,1.02-1.41,1.41l2.18,2.18,1.41-1.41-2.18-2.18Z"/>
+        <path fill="currentColor" d="m14.91,8c.05.33.09.66.09,1s-.04.67-.09,1h3.09v-2h-3.09Z"/>
+        <path fill="currentColor" d="m13.89,5.53l2.18-2.18-1.41-1.41-2.18,2.18c.55.39,1.02.87,1.41,1.41Z"/>
+</svg>
+
+
+
+<style lang="scss"></style>
