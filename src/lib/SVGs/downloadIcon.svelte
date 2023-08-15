58565ade1fdf1029b7f3c85c87f785294ed8df10
Richard Fu <richardfxr.personal@gmail.com>
2023-08-15 15:14:05 -0700

    📐DES: create download & info icons

---
+++ src/lib/SVGs/downloadIcon.svelte
@@ -0,0 +1,11 @@
+<svg
+    class="download icon"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 18 18">
+    <path fill="currentColor" d="m16,17H2c-1.1,0-2-.9-2-2v-4h2v4h14v-4h2v4c0,1.1-.9,2-2,2Z"/>
+    <path fill="currentColor" d="m14.29,5.29l-4.29,4.29V0h-2v9.59L3.71,5.29l-1.41,1.41,5.29,5.29c.39.39.9.58,1.41.58s1.02-.19,1.41-.58l5.29-5.29-1.41-1.41Z"/>
+</svg>
+
+
+
+<style lang="scss"></style>
---
+++ src/lib/SVGs/infoIcon.svelte
@@ -0,0 +1,12 @@
+<svg
+    class="info icon"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 18 18">
+    <path fill="currentColor" d="m9,18C4.04,18,0,13.96,0,9S4.04,0,9,0s9,4.04,9,9-4.04,9-9,9Zm0-16c-3.86,0-7,3.14-7,7s3.14,7,7,7,7-3.14,7-7-3.14-7-7-7Z"/>
+    <rect fill="currentColor" x="8" y="8" width="2" height="6"/>
+    <rect fill="currentColor" x="8" y="4" width="2" height="2"/>
+</svg>
+
+
+
+<style lang="scss"></style>
