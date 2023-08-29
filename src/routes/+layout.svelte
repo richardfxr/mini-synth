36615e85489a5c48f730c22ec356ba147c4aac68
Richard Fu <richardfxr.personal@gmail.com>
2023-08-28 17:48:33 -0700

    🟠SVLT: add info console logs

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -29,6 +29,27 @@
 
     /* === LIFECYCLES ========================= */
     onMount(async () => {
+        // console log styling
+        const note0 = "background-color: #fad94d; color: black";
+        const note1 = "background-color: #ffb566; color: black";
+        const note2 = "background-color: #ff9a85; color: black";
+        const note3 = "background-color: #ff85a1; color: black";
+        const note4 = "background-color: #f090f7; color: black";
+        const note5 = "background-color: #d1a1ff; color: black";
+        const note6 = "background-color: #b0b7ff; color: black";
+        const note7 = "background-color: #9ec8ff; color: black";
+        const note8 = "background-color: #7cd7f7; color: black";
+        const note9 = "background-color: #85e6d3; color: black";
+        const note10 = "background-color: #7fe3b1; color: black";
+        const note11 = "background-color: #a4eb7a; color: black";
+        // console logs
+        console.log("");
+        console.log("%c %cm%ci%cn%ci%c %cs%cy%cn%ct%ch%c ", note0, note1, note2, note3, note4, note5, note6, note7, note8, note9, note10, note11);
+        console.log("v1.1.0");
+        console.log("Source code available on GitHub:");
+        console.log("https://github.com/richardfxr/mini-synth");
+        console.log("");
+
         // colorScheme setting
         const dbColorScheme = await db.settings.get("colorScheme");
         dbColorScheme ? colorScheme.set(dbColorScheme.value) : colorScheme.set("auto");
